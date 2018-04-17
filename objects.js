173d0bae8b2cc542b9dff2562401f0747d1b4744
Jennifer Snyder <jlsnyder09@gmail.com>
2018-04-17 00:31:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,7 @@
-var playlist = {"artist name": "song title"}
+var playlist = {"artistName": "songTitle"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  Object.assign({playlist}, {artistName: songTitle});
+  return playlist
+}
+
